efee953bff048e70a28017f233d578607071bf50
Kris Leech <kris.leech@gmail.com>
2015-11-14 11:48:36 +0000

    fix difference with specs on CI ActiveRecord 3 and 4 have slightly different error messages

--- spec/not_found_spec.rb
+++ spec/not_found_spec.rb
@@ -16,9 +16,7 @@ describe User, 'model' do
       end
 
       it 'raises User::RecordNotFound with expected message' do
-        # FIXME: why different on CI
-        expected_message = ENV.has_key?('CI') ? "Couldn't find User with id=1" : "Couldn't find User with 'id'=1"
-        expect { User.find(1) }.to raise_error(User::RecordNotFound, expected_message)
+        expect { User.find(1) }.to raise_error(User::RecordNotFound, /Couldn't find User/)
       end
     end
   end
